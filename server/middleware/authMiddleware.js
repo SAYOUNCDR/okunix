6ce26338f5e34354ca61e1962747084e6ec6200f
0xSYN.dev <pheonixgamer12345@gmail.com>
2026-01-22 01:09:43 +0530

    feat(middleware): add auth token verify middleware

---
+++ server/middleware/authMiddleware.js
@@ -0,0 +1,19 @@
+const jwt = require("jsonwebtoken");
+
+const verifyToken = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+        return res.status(401).json({ message: "Unauthorized: No token provided" });
+    }
+    const token = authHeader.split(" ")[1]; // this comes as Bearer <token> in btw space
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        if (err) {
+            return res.status(403).json({ message: "Forbidden" });
+        }
+        req.user = user;
+        next();
+    });
+}
+
+module.exports = verifyToken;
