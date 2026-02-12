624a9a8b14e9ad39b0dc0a8b4fc28b763136029e
0xSYN.dev <pheonixgamer12345@gmail.com>
2026-02-12 13:24:36 +0530

    feat: add role in user model

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -36,7 +36,9 @@ exports.register = async (req, res) => {
 
     const existingUser = await User.findOne({ email });
     if (existingUser) {
-      return res.status(400).json({ message: "User already exists" });
+      return res
+        .status(400)
+        .json({ message: "Registration failed: User already exists" });
     }
 
     const hashedPassword = await bcrypt.hash(password, 10);
