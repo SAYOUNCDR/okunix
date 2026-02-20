269f7d569e774aefb003b2870456b4d90fcce072
0xSYN.dev <pheonixgamer12345@gmail.com>
2026-02-21 00:17:16 +0530

    feat(server): add delete account controller & route

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -257,3 +257,20 @@ exports.getMe = async (req, res) => {
     res.status(500).json({ message: "Internal server error" });
   }
 };
+
+
+exports.deleteAccount = async (req, res) => {
+  try {
+    const userId = req.user.id;
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    await User.findByIdAndDelete(userId);
+    res.status(200).json({ message: "Account deleted successfully" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
