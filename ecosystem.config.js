2fe01d3fe737094f33fc3252652f7932d66cc1a3
0xSYN.dev <pheonixgamer12345@gmail.com>
2026-02-26 10:52:25 +0530

    feat: add new cicd

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -10,18 +10,18 @@ module.exports = {
                 NODE_ENV: "production",
             }
         },
-        {
-            name: "okunix-frontend",
-            script: "serve",
-            // CHANGE THIS LINE:
-            env: {
-                PM2_SERVE_PATH: '/home/azureuser/projects/okunix/client-dist',
-                PM2_SERVE_PORT: 5173,
-                PM2_SERVE_SPA: 'true',
-                PM2_SERVE_MAX_AGE: 31536000,
-                NODE_ENV: "production"
-            }
-        }
+        // {
+        //     name: "okunix-frontend",
+        //     script: "serve",
+        //     // CHANGE THIS LINE:
+        //     env: {
+        //         PM2_SERVE_PATH: '/home/azureuser/projects/okunix/client-dist',
+        //         PM2_SERVE_PORT: 5173,
+        //         PM2_SERVE_SPA: 'true',
+        //         PM2_SERVE_MAX_AGE: 31536000,
+        //         NODE_ENV: "production"
+        //     }
+        // }
     ]
 };
 
